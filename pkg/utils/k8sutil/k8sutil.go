8b2c951755c0ceda19d36f9cd05563108a0c95b8
David Maddison <david.maddison@emc.com>
2018-08-07 12:56:30 +0100

    Issue 22: Allow blank watch namespace

--- pkg/utils/k8sutil/k8sutil.go
+++ pkg/utils/k8sutil/k8sutil.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 
 	"github.com/operator-framework/operator-sdk/pkg/k8sclient"
+	"github.com/operator-framework/operator-sdk/pkg/util/k8sutil"
 	api "github.com/pravega/pravega-operator/pkg/apis/pravega/v1alpha1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"os"
 )
 
 func AsOwnerRef(pravegaCluster *api.PravegaCluster) *metav1.OwnerReference {
@@ -28,4 +30,13 @@ func DeleteCollection(apiVersion string, kind string, namespace string, labels s
 	return resourceClient.DeleteCollection(nil, metav1.ListOptions{
 		LabelSelector: labels,
 	})
+}
+
+// GetWatchNamespaceAllowBlank returns the namespace the operator should be watching for changes
+func GetWatchNamespaceAllowBlank() (string, error) {
+	ns, found := os.LookupEnv(k8sutil.WatchNamespaceEnvVar)
+	if !found {
+		return "", fmt.Errorf("%s must be set", k8sutil.WatchNamespaceEnvVar)
+	}
+	return ns, nil
 }
