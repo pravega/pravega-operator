9deaf1ede36832a0675c7ab59cec3c6153183f26
Olivier Bourgeois <olivier.bourgeois@emc.com>
2018-10-25 11:50:37 -0300

    Print errors on all component creation failures
    Signed-off-by: Olivier Bourgeois <olivier.bourgeois@emc.com>

--- pkg/pravega/pravega_segmentstore.go
+++ pkg/pravega/pravega_segmentstore.go
@@ -36,13 +36,11 @@ const (
 func deploySegmentStore(pravegaCluster *api.PravegaCluster) (err error) {
 	err = sdk.Create(makeSegmentstoreConfigMap(pravegaCluster))
 	if err != nil && !errors.IsAlreadyExists(err) {
-		logrus.Error(err)
 		return err
 	}
 
 	err = sdk.Create(makeSegmentStoreStatefulSet(pravegaCluster))
 	if err != nil && !errors.IsAlreadyExists(err) {
-		logrus.Error(err)
 		return err
 	}
 
