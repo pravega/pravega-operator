aff97be4ea7d7cf334541f0236d60fe3c0197a14
Adrian Moreno <adrian@morenomartinez.com>
2018-12-03 12:52:28 +0100

    Create e2e testing framework
    Signed-off-by: Adrian Moreno <adrian@morenomartinez.com>

---
+++ test/e2e/basic_test.go
@@ -0,0 +1,12 @@
+package e2e
+
+import (
+	"testing"
+
+	framework "github.com/operator-framework/operator-sdk/pkg/test"
+)
+
+func testCreateCluster(t *testing.T, f *framework.Framework, ctx *framework.TestCtx) error {
+	t.Skip("Not implemented")
+	return nil
+}
