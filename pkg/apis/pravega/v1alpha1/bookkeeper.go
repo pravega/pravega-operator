4308f522a189207c8ec4447f7ac07a5971b8014d
Adrián Moreno <adrian@morenomartinez.com>
2019-03-20 16:02:05 +0100

    Reduce default resource consumption
    Signed-off-by: Adrián Moreno <adrian@morenomartinez.com>

--- pkg/apis/pravega/v1alpha1/bookkeeper.go
+++ pkg/apis/pravega/v1alpha1/bookkeeper.go
@@ -111,11 +111,11 @@ func (s *BookkeeperSpec) withDefaults() (changed bool) {
 		s.Resources = &v1.ResourceRequirements{
 			Requests: v1.ResourceList{
 				v1.ResourceCPU:    resource.MustParse("1000m"),
-				v1.ResourceMemory: resource.MustParse("3Gi"),
+				v1.ResourceMemory: resource.MustParse("1Gi"),
 			},
 			Limits: v1.ResourceList{
 				v1.ResourceCPU:    resource.MustParse("2000m"),
-				v1.ResourceMemory: resource.MustParse("5Gi"),
+				v1.ResourceMemory: resource.MustParse("2Gi"),
 			},
 		}
 	}
--- pkg/apis/pravega/v1alpha1/pravega.go
+++ pkg/apis/pravega/v1alpha1/pravega.go
@@ -146,12 +146,12 @@ func (s *PravegaSpec) withDefaults() (changed bool) {
 		changed = true
 		s.ControllerResources = &v1.ResourceRequirements{
 			Requests: v1.ResourceList{
-				v1.ResourceCPU:    resource.MustParse("1000m"),
-				v1.ResourceMemory: resource.MustParse("1Gi"),
+				v1.ResourceCPU:    resource.MustParse("500m"),
+				v1.ResourceMemory: resource.MustParse("512Mi"),
 			},
 			Limits: v1.ResourceList{
-				v1.ResourceCPU:    resource.MustParse("2000m"),
-				v1.ResourceMemory: resource.MustParse("3Gi"),
+				v1.ResourceCPU:    resource.MustParse("1000m"),
+				v1.ResourceMemory: resource.MustParse("1Gi"),
 			},
 		}
 	}
@@ -161,11 +161,11 @@ func (s *PravegaSpec) withDefaults() (changed bool) {
 		s.SegmentStoreResources = &v1.ResourceRequirements{
 			Requests: v1.ResourceList{
 				v1.ResourceCPU:    resource.MustParse("1000m"),
-				v1.ResourceMemory: resource.MustParse("3Gi"),
+				v1.ResourceMemory: resource.MustParse("1Gi"),
 			},
 			Limits: v1.ResourceList{
 				v1.ResourceCPU:    resource.MustParse("2000m"),
-				v1.ResourceMemory: resource.MustParse("5Gi"),
+				v1.ResourceMemory: resource.MustParse("2Gi"),
 			},
 		}
 	}
