7711e665dd3fb4c0b827707d4f4b2603e7d98d56
wenqimou <452787782@qq.com>
2019-07-23 01:18:42 -0700

    Add svc cleanup
    Signed-off-by: wenqimou <452787782@qq.com>

--- pkg/webhook/add_webhook.go
+++ pkg/webhook/add_webhook.go
@@ -11,9 +11,19 @@
 package webhook
 
 import (
+	"context"
+	"fmt"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/intstr"
 	"log"
 	"os"
-
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
@@ -61,6 +71,10 @@ func Add(mgr manager.Manager) error {
 	}
 
 	svr.Register(wh)
+	err = createWebhookK8sService(mgr)
+	if err != nil {
+		log.Printf("Failed to create webhook svc: %v", err)
+	}
 	return nil
 }
 
@@ -77,15 +91,50 @@ func newValidatingWebhook(mgr manager.Manager) (*admission.Webhook, error) {
 func newWebhookServer(mgr manager.Manager) (*webhook.Server, error) {
 	return webhook.NewServer("pravega-admission-webhook", mgr, webhook.ServerOptions{
 		CertDir: CertDir,
-		BootstrapOptions: &webhook.BootstrapOptions{
-			// TODO: garbage collect webhook k8s service
-			Service: &webhook.Service{
-				Namespace: os.Getenv("WATCH_NAMESPACE"),
-				Name:      "pravega-admission-webhook",
-				Selectors: map[string]string{
-					"component": "pravega-operator",
+	})
+}
+
+func createWebhookK8sService(mgr manager.Manager) error {
+	cfg, err := config.GetConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+	c, _ := client.New(cfg, client.Options{Scheme: mgr.GetScheme()})
+	// create webhook k8s service
+	svc := &corev1.Service{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Service",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "pravega-admission-webhook",
+			Namespace: os.Getenv("WATCH_NAMESPACE"),
+		},
+		Spec: corev1.ServiceSpec{
+			Selector: map[string]string{
+				"component": "pravega-operator",
+			},
+			Ports: []corev1.ServicePort{
+				{
+					// When using service, kube-apiserver will send admission request to port 443.
+					Port:       443,
+					TargetPort: intstr.IntOrString{Type: intstr.Int, IntVal: 443},
 				},
 			},
 		},
-	})
+	}
+	// get operator deployment
+	nn := types.NamespacedName{Namespace: os.Getenv("WATCH_NAMESPACE"), Name: os.Getenv("OPERATOR_NAME")}
+	deployment := &appsv1.Deployment{}
+	err = c.Get(context.TODO(), nn, deployment)
+	if err != nil {
+		return fmt.Errorf("failed to get operator deployment: %v", err)
+	}
+	// add owner reference
+	controllerutil.SetControllerReference(deployment, svc, mgr.GetScheme())
+	err = c.Create(context.TODO(), svc)
+	if err != nil && !errors.IsAlreadyExists(err) {
+		return fmt.Errorf("failed to create webhook k8s service: %v", err)
+	}
+	return nil
 }
