7f3d31ba6b8c8b9b793fc3482a190c994ea8d984
wenqimou <452787782@qq.com>
2019-07-29 18:06:39 -0700

    Fix unit test and README
    Signed-off-by: wenqimou <452787782@qq.com>

--- pkg/webhook/add_webhook.go
+++ pkg/webhook/add_webhook.go
@@ -95,10 +95,6 @@ func newMutatingWebhook(mgr manager.Manager) (*admission.Webhook, error) {
 }
 
 func newWebhookServer(mgr manager.Manager) (*webhook.Server, error) {
-	namespace, err := k8sutil.GetOperatorNamespace()
-	if err != nil {
-		return nil, err
-	}
 	return webhook.NewServer(WebhookSvcName, mgr, webhook.ServerOptions{
 		CertDir: CertDir,
 		BootstrapOptions: &webhook.BootstrapOptions{
