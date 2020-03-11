b653dba8549deeaa6cc9c0eea86a4447ce3d4f03
Srishti Thakkar <Srishti.Thakkar@emc.com>
2020-03-12 00:33:06 +0530

    Issue 294: Script for Pravega Cluster deployment along with dependencies (#321)
    * Issue 294: Script for Pravega Cluster deployment
    
    Signed-off-by: SrishT <Srishti.Thakkar@dell.com>
    
    * Issue 294: Implementing review comments
    
    Signed-off-by: SrishT <Srishti.Thakkar@dell.com>
    
    * Issue 294: Implementing review comments
    
    Signed-off-by: SrishT <Srishti.Thakkar@dell.com>
    
    * Issue 294: Updating README
    
    Signed-off-by: SrishT <Srishti.Thakkar@dell.com>
    
    Co-authored-by: SrishT <Srishti.Thakkar@dell.com>

---
+++ setup/README.md
@@ -0,0 +1,40 @@
+# Script for complete Pravega Installation
+
+The purpose of this script is to sequentially install all the dependencies (i.e. Operators, Zookeeper and Bookkeeper) and Pravega Components in the right order.
+
+## Prerequisites
+
+  - Kubernetes 1.10+ with Beta APIs
+  - Helm 2.10+
+  - [Tier 2 Setup](https://github.com/pravega/pravega-operator#set-up-tier-2-storage)
+  - Copy the necessary charts to the right location
+
+First clone the [Zookeeper Operator](https://github.com/pravega/zookeeper-operator) and [Bookkeeper Operator](https://github.com/pravega/bookkeeper-operator) repositories locally using :
+```
+git clone https://github.com/pravega/zookeeper-operator
+git clone https://github.com/pravega/bookkeeper-operator
+```
+
+Next, copy the contents of the charts directory from both these repositories inside the charts directory of this repository.
+```
+cp -r <path-to-zookeeper-operator-repo>/charts/ <path-to-pravega-operator-repo>/charts/.
+cp -r <path-to-bookkeeper-operator-repo>/charts/ <path-to-pravega-operator-repo>/charts/.
+```
+
+This will result in separate sub-directories for [zookeeper-operator](https://github.com/pravega/zookeeper-operator/tree/master/charts/zookeeper-operator), [zookeeper](https://github.com/pravega/zookeeper-operator/tree/master/charts/zookeeper), [bookkeeper-operator](https://github.com/pravega/bookkeeper-operator/tree/master/charts/pravega-operator) and [bookkeeper](https://github.com/pravega/bookkeeper-operator/tree/master/charts/pravega) charts alongside the directories for [pravega-operator](https://github.com/pravega/pravega-operator/tree/master/charts/pravega-operator) and [pravega](https://github.com/pravega/pravega-operator/tree/master/charts/pravega) charts inside the [charts](https://github.com/pravega/pravega-operator/tree/master/charts) directory.
+
+## Installing the Pravega Cluster
+
+To install the Pravega Cluster along with all the required dependencies, run the following command:
+
+```
+$ ./pravegacluster.sh install
+```
+
+## Uninstalling the Pravega Cluster
+
+To uninstall the Pravega Cluster along with all its dependencies, run the following command:
+
+```
+$ ./pravegacluster.sh delete
+```
---
+++ setup/pravegacluster.sh
@@ -0,0 +1,96 @@
+#!/usr/bin/env bash
+
+if [ "$#" -ne 1 ]; then
+  echo "Error : Invalid number of arguments"
+  echo "Usage: ./pravegacluster.sh [install/delete]"
+	exit 1
+fi
+
+jsonpath1="{.spec.replicas}"
+jsonpath2="{.status.readyReplicas}"
+zk_opr_name=zoo-op
+zk_name=zoo
+bk_opr_name=pr
+bk_name=br
+pr_opr_name=foo
+pr_name=bar
+
+install_cluster () {
+  zk_chart=$(cat ../charts/zookeeper/Chart.yaml | grep name:)
+  arr=(`echo ${zk_chart}`)
+  zk_chart_name=${arr[1]}
+
+  bk_chart=$(cat ../charts/bookkeeper/Chart.yaml | grep name:)
+  arr=(`echo ${bk_chart}`)
+  bk_chart_name=${arr[1]}
+
+	helm install ../charts/zookeeper-operator --name $zk_opr_name
+	kubectl rollout status deploy/$zk_opr_name-zookeeper-operator
+
+	helm install ../charts/zookeeper --name $zk_name
+
+	replicas=$(kubectl get zookeepercluster $zk_name-$zk_chart_name -o jsonpath=$jsonpath1)
+	ready=$(kubectl get zookeepercluster $zk_name-$zk_chart_name -o jsonpath=$jsonpath2)
+
+	until [ "$replicas" == "$ready" ]
+	do
+		sleep 1;
+		replicas=$(kubectl get zookeepercluster $zk_name-$zk_chart_name -o jsonpath=$jsonpath1)
+		ready=$(kubectl get zookeepercluster $zk_name-$zk_chart_name -o jsonpath=$jsonpath2)
+	done
+
+	zk_svc_details=$(kubectl get svc | grep $zk_name- | grep client)
+  arr=(`echo ${zk_svc_details}`)
+  zk_svc=${arr[0]}
+  zk_port_name=${arr[4]}
+  IFS="/" read -a arr <<< "$zk_port_name"
+  zk_port=${arr[0]}
+
+	helm install ../charts/bookkeeper-operator --name $bk_opr_name
+	kubectl rollout status deploy/$bk_opr_name-bookkeeper-operator
+
+	helm install ../charts/bookkeeper --name $bk_name --set zookeeperUri=$zk_svc:$zk_port
+
+	replicas=$(kubectl get bookkeepercluster $bk_name-$bk_chart_name -o jsonpath=$jsonpath1)
+	ready=$(kubectl get bookkeepercluster $bk_name-$bk_chart_name -o jsonpath=$jsonpath2)
+
+	until [ "$replicas" == "$ready" ]
+	do
+		sleep 1;
+		replicas=$(kubectl get bookkeepercluster $bk_name-$bk_chart_name -o jsonpath=$jsonpath1)
+		ready=$(kubectl get bookkeepercluster $bk_name-$bk_chart_name -o jsonpath=$jsonpath2)
+	done
+
+  bk_svc_details=$(kubectl get svc | grep $bk_name- | grep headless)
+  arr=(`echo ${bk_svc_details}`)
+  bk_svc=${arr[0]}
+  bk_port_name=${arr[4]}
+  IFS="/" read -a arr <<< "$bk_port_name"
+  bk_port=${arr[0]}
+
+	helm install ../charts/pravega-operator --name $pr_opr_name
+	kubectl rollout status deploy/$pr_opr_name-pravega-operator
+
+	helm install ../charts/pravega --name $pr_name --set zookeeperUri=$zk_svc:$zk_port --set bookkeeperUri=$bk_svc:$bk_port
+}
+
+delete_cluster() {
+  helm del $pr_name --purge
+	helm del $bk_name --purge
+	helm del $zk_name --purge
+	helm del $pr_opr_name --purge
+	helm del $bk_opr_name --purge
+	helm del $zk_opr_name --purge
+}
+
+if [ $1 == "install" ]; then
+  install_cluster
+
+elif [ $1 == "delete" ]; then
+	delete_cluster
+
+else
+	echo "Error: Invalid argument"
+  echo "Use [install] to install the cluster or [delete] to remove the existing setup"
+	exit 1
+fi
