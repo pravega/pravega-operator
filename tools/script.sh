7422bfcccb812e905a8e8d8d611ddaf1365f4048
prabhaker24 <prabhaker.saxena@dell.com>
2020-04-15 16:12:27 +0530

    script changed
    Signed-off-by: prabhaker24 <prabhaker.saxena@dell.com>

--- tools/script.sh
+++ tools/script.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 set -ex
 
-echo "This is a pre upgrade script required for upgradig pravega operator <= 0.4.x to >=0.5.x "
+echo "Running pre-upgrade script for upgrading pravega operator from version 0.4.x to 0.5.0"
 
 if [ "$#" -ne 2 ]; then
 	echo "Error : Invalid number of arguments"
-	echo "Usage: ./script.sh namespace(pravega operator) name(pravega cluster)"
+	echo "Usage: ./script.sh namespace_used_for_pravega-operator name_used_for_pravega-cluster"
 	exit 1
 fi
 
@@ -15,19 +15,33 @@ local namespace=$1
 
 local name=$2
 
+kubectl apply -f ./manifest_files/cert-manager.yaml
+
+sed -i "s/namespace.*/namespace: $namespace"/ ./manifest_files/secret.yaml
+
+kubectl apply -f  ./manifest_files/secret.yaml
+
 sed -i "s|cert.*|cert-manager.io/inject-ca-from: $namespace/selfsigned-cert|" ./manifest_files/webhook.yaml
 
 sed -i "s/namespace.*/namespace: $namespace "/ ./manifest_files/webhook.yaml
 
 kubectl apply -f ./manifest_files/webhook.yaml
 
+sed -i "s/namespace.*/namespace: $namespace "/ ./manifest_files/version_map.yaml
+
 kubectl apply -f  ./manifest_files/version_map.yaml
 
-kubectl apply -f "https://github.com/jetstack/cert-manager/releases/download/v0.14.1/cert-manager.yaml"
+cabundle=`kubectl get ValidatingWebhookConfiguration pravega-webhook-config --output yaml | grep caBundle: | awk '{print $2}'`
 
-sed -i "s/namespace.*/namespace: $namespace"/ ./manifest_files/secret.yaml
+sed -i "s/caBundle.*/caBundle: $cabundle "/ ./manifest_files/crd.yaml
 
-kubectl apply -f  ./manifest_files/secret.yaml
+sed -i "s/namespace.*/namespace: $namespace "/ ./manifest_files/crd.yaml
+
+kubectl apply -f  ./manifest_files/crd.yaml
+
+sed -i "s/namespace.*/namespace: $namespace "/ ./manifest_files/role.yaml
+
+kubectl apply -f ./manifest_files/role.yaml
 
 op_name=`kubectl get deployment | grep "pravega-operator" | awk '{print $1}'`
 
@@ -47,21 +61,13 @@ sed -i "s|image:.*|image: $op_image|" ./manifest_files/operator.yaml
 
 kubectl apply -f ./manifest_files/operator.yaml
 
-cabundle=`kubectl get ValidatingWebhookConfiguration pravega-webhook-config --output yaml | grep caBundle: | awk '{print $2}'`
-
-sed -i "s/caBundle.*/caBundle: $cabundle "/ ./manifest_files/crd.yaml
-
-sed -i "s/namespace.*/namespace: $namespace "/ ./manifest_files/crd.yaml
-
-kubectl apply -f  ./manifest_files/crd.yaml
-
 sed -i "s/name.*/name: ${name}-bk-supported-upgrade-paths"/ ./manifest_files/bk_version_map.yaml
+
+sed -i "s/namespace.*/namespace: $namespace "/ ./manifest_files/bk_version_map.yaml
 
 kubectl apply -f  ./manifest_files/bk_version_map.yaml
 
 helm install charts/bookkeeper-operator --name bkop --namespace $namespace
-
-kubectl apply -f ./manifest_files/role.yaml
 
 }
 
