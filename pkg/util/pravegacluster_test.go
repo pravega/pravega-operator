ce765d2296b58f4aa2a9ee8d916791931f19ea65
prabhaker24 <prabhaker.saxena@dell.com>
2020-06-17 12:22:37 +0530

    added ut
    Signed-off-by: prabhaker24 <prabhaker.saxena@dell.com>

--- pkg/util/pravegacluster_test.go
+++ pkg/util/pravegacluster_test.go
@@ -175,6 +175,12 @@ var _ = Describe("pravegacluster", func() {
 		})
 
 	})
+	Context("ReadinessHealthcheckCommand()", func() {
+		out := ReadinessHealthcheckCommand(1234)
+		It("Should not be Empty", func() {
+			Ω(len(out)).ShouldNot(Equal(0))
+		})
+	})
 	Context("Min()", func() {
 
 		It("Min should be 10", func() {
