b4d0e37a7b43712490726fd2b99e6e72b80ebfda
SrishT <Srishti.Thakkar@dell.com>
2020-08-19 17:42:13 +0530

    Issue 439: Addressing review comments
    Signed-off-by: SrishT <Srishti.Thakkar@dell.com>

--- scripts/post-upgrade.sh
+++ scripts/post-upgrade.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 set -ex
 
-if [ "$#" -ne 6 ]; then
+if [[ "$#" -lt 4 || "$#" -gt 6 ]]; then
 	echo "Error : Invalid number of arguments"
 	Usage: "./post-upgrade.sh <pravegacluster name> <pravega-release-name> <bookkeeper-release-name> <version> <namespace> <zk-svc-name>"
 	exit 1
@@ -11,27 +11,31 @@ name=$1
 pname=$2
 bkname=$3
 version=$4
-namespace=$5
-zksvc=$6
+namespace=${5:-default}
+zksvc=${6:-zookeeper-client}
 
+echo "Checking that the PravegaCluster resource is in ready state"
 kubectl describe PravegaCluster $name -n $namespace
 currentReplicasPc=`kubectl get PravegaCluster $name -n $namespace -o jsonpath='{.status.currentReplicas}'`
 readyReplicasPc=`kubectl get PravegaCluster $name -n $namespace -o jsonpath='{.status.readyReplicas}'`
 if [ $currentReplicasPc != $readyReplicasPc ]; then
+	echo "Error : Pravega Cluster is not in ready state"
   exit 1
 fi
-## Add annotations and labels on PravegaCluster resources to make sure they're owned by the right chart.
+echo "Adding required annotations and labels to the PravegaCluster resource to make sure they're owned by the right chart"
 kubectl annotate PravegaCluster $name meta.helm.sh/release-name=$pname -n $namespace --overwrite
 kubectl annotate PravegaCluster $name meta.helm.sh/release-namespace=$namespace -n $namespace --overwrite
 kubectl label PravegaCluster $name app.kubernetes.io/managed-by=Helm -n $namespace --overwrite
 
+echo "Checking that the BookkeeperCluster resource is in ready state"
 kubectl get BookkeeperCluster $name -n $namespace
 currentReplicasBk=`kubectl get BookkeeperCluster $name -n $namespace -o jsonpath='{.status.replicas}'`
 readyReplicasBk=`kubectl get BookkeeperCluster $name -n $namespace -o jsonpath='{.status.readyReplicas}'`
 if [ $currentReplicasBk != $readyReplicasBk ]; then
+	echo "Error : Bookkeeper Cluster is not in ready state"
   exit 2
 fi
-## Add annotations and labels on BookkeeperCluster resources to make sure they're owned by the right chart.
+echo "Adding required annotations and labels to the BookkeeperCluster resource to make sure they're owned by the right chart"
 kubectl annotate BookkeeperCluster $name meta.helm.sh/release-name=$bkname -n $namespace --overwrite
 kubectl annotate BookkeeperCluster $name meta.helm.sh/release-namespace=$namespace -n $namespace --overwrite
 kubectl label BookkeeperCluster $name app.kubernetes.io/managed-by=Helm -n $namespace --overwrite
@@ -39,7 +43,7 @@ kubectl annotate ConfigMap pravega-config meta.helm.sh/release-name=$bkname -n $
 kubectl annotate ConfigMap pravega-config meta.helm.sh/release-namespace=$namespace -n $namespace --overwrite
 kubectl label ConfigMap pravega-config app.kubernetes.io/managed-by=Helm -n $namespace --overwrite
 
-## Install the bookkeeper charts
+echo "Installing / Upgrading the pravega and bookkeeper charts"
 helm repo add pravega https://charts.pravega.io
 helm repo update
 helm upgrade $pname pravega/pravega --version=$version --set fullnameOverride=$name --set zookeeperUri="$zksvc:2181" --set bookkeeperUri="$name-bookie-headless:3181"
