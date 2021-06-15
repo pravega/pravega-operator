c6c9ecf47ac0d8a3403ab4d00e36500ce7b715c3
anishakj <anisha.kj@dell.com>
2021-06-15 00:29:31 -0700

    Updating scripts to use specific docker version
    Signed-off-by: anishakj <anisha.kj@dell.com>

--- test/e2e/resources/kubernetes_slave_install.sh
+++ test/e2e/resources/kubernetes_slave_install.sh
@@ -7,23 +7,30 @@ sudo rm /lib/systemd/system/nfs-common.service
 sudo systemctl daemon-reload
 sudo systemctl start nfs-common
 sudo systemctl status nfs-common
-sudo apt install docker.io -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable"
+sudo apt-get update
+echo "update done"
+sudo apt-get install docker-ce=5:19.03.9~3-0~ubuntu-focal -y
 echo "docker install"
 sudo systemctl enable --now docker
 apt-get update && apt-get install -y \
-  apt-transport-https ca-certificates curl software-properties-common gnupg2 
+  apt-transport-https ca-certificates curl software-properties-common gnupg2
 echo "installed certs"
 sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" \
   | sudo tee -a /etc/apt/sources.list.d/kubernetes.list \
-  && sudo apt-get update 
+  && sudo apt-get update
 sudo apt-get update \
   && sudo apt-get install -yq \
   kubelet \
   kubeadm \
   kubernetes-cni
 sudo apt-mark hold kubelet kubeadm kubectl
-UUID=`cat /etc/fstab | grep swap | awk '{print $1}' | tr -d "#UUID="` 
+UUID=`cat /etc/fstab | grep swap | awk '{print $1}' | tr -d "#UUID="`
 sed -i '2 s/^/#/' /etc/fstab
 echo "swapoff UUID=$UUID"
 swapoff UUID=$UUID
