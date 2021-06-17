bb5274652beabf5c4b8cbd5cd493528e03fd59e3
anishakj <43978302+anishakj@users.noreply.github.com>
2021-06-17 12:44:53 +0530

    Issue 558: Fixing end to end tests  (#559)
    * Updating scripts to use specific docker version
    
    Signed-off-by: anishakj <anisha.kj@dell.com>
    
    * commenting out deletion of cluster
    
    Signed-off-by: anishakj <anisha.kj@dell.com>
    
    * testing e2e
    
    Signed-off-by: anishakj <anisha.kj@dell.com>
    
    * changing cluster location
    
    Signed-off-by: anishakj <anisha.kj@dell.com>
    
    * changing cluster location
    
    Signed-off-by: anishakj <anisha.kj@dell.com>
    
    * changing cluster location
    
    Signed-off-by: anishakj <anisha.kj@dell.com>
    
    * changing cluster location
    
    Signed-off-by: anishakj <anisha.kj@dell.com>
    
    * changing cluster location
    
    Signed-off-by: anishakj <anisha.kj@dell.com>
    
    * Uncommenting delete clusters
    
    Signed-off-by: anishakj <anisha.kj@dell.com>

--- test/e2e/resources/kubernetes_master_install.sh
+++ test/e2e/resources/kubernetes_master_install.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
-echo "TCPKeepAlive yes" >> /etc/ssh/sshd_config 
+echo "TCPKeepAlive yes" >> /etc/ssh/sshd_config
 echo "ClientAliveInterval 60" >> /etc/ssh/sshd_config
 echo "ClientAliveCountMax 3" >> /etc/ssh/sshd_config
 service sshd restart
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+   stable"
 sudo apt-get update
 echo "update done"
-sudo apt install docker.io -y
+sudo apt-get install docker-ce=5:19.03.9~3-0~ubuntu-focal -y
 echo "docker install"
 sudo systemctl enable --now docker
 sudo apt-get install nfs-common -y
@@ -14,29 +19,29 @@ sudo systemctl daemon-reload
 sudo systemctl start nfs-common
 sudo systemctl status nfs-common
 apt-get update && apt-get install -y \
-  apt-transport-https ca-certificates curl software-properties-common gnupg2 
+  apt-transport-https ca-certificates curl software-properties-common gnupg2
 echo "installed certs"
 sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" \
   | sudo tee -a /etc/apt/sources.list.d/kubernetes.list \
-  && sudo apt-get update 
+  && sudo apt-get update
 sudo apt-get update \
   && sudo apt-get install -yq \
   kubelet \
   kubeadm \
   kubernetes-cni
 sudo apt-mark hold kubelet kubeadm kubectl
-UUID=`cat /etc/fstab | grep swap | awk '{print $1}' | tr -d "#UUID="` 
+UUID=`cat /etc/fstab | grep swap | awk '{print $1}' | tr -d "#UUID="`
 sed -i '2 s/^/#/' /etc/fstab
 echo "swapoff UUID=$UUID"
 swapoff UUID=$UUID
 IP=`ifconfig bond0:0 | grep "inet" | awk '{print $2}'`
-sudo kubeadm init --apiserver-advertise-address=$IP
+sudo kubeadm init --apiserver-advertise-address=$IP  --pod-network-cidr=192.168.0.0/16
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 kubectl get nodes
 kubectl apply -f https://docs.projectcalico.org/v3.14/manifests/calico.yaml
-kubectl get nodes 
+kubectl get nodes
 sudo apt-get install binutils bison gcc -y
 mkdir /export
