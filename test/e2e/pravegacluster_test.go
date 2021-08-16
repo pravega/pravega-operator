c29124cc204aadcc76b79913c15c7ab3749b2f0e
SrishT <Srishti.Thakkar@dell.com>
2021-08-16 23:29:15 +0530

    Issue 357: Testing
    Signed-off-by: SrishT <Srishti.Thakkar@dell.com>

--- test/e2e/pravegacluster_test.go
+++ test/e2e/pravegacluster_test.go
@@ -57,15 +57,15 @@ func testPravegaCluster(t *testing.T) {
 	}
 
 	testFuncs := map[string]func(t *testing.T){
-		// "testCreateRecreateCluster":         testCreateRecreateCluster,
-		// "testScaleCluster":                  testScaleCluster,
-		// "testUpgradeCluster":                testUpgradeCluster,
-		// "testWebhook":                       testWebhook,
-		// "testCMUpgradeCluster":              testCMUpgradeCluster,
-		// "testExternalCreateRecreateCluster": testExternalCreateRecreateCluster,
+		"testScaleCluster":                  testScaleCluster,
+		"testCreateRecreateCluster":         testCreateRecreateCluster,
+		"testUpgradeCluster":                testUpgradeCluster,
+		"testWebhook":                       testWebhook,
+		"testCMUpgradeCluster":              testCMUpgradeCluster,
+		"testExternalCreateRecreateCluster": testExternalCreateRecreateCluster,
 		// commenting out this test as pravega installation with TLS alone is not supported
-		//"testCreatePravegaClusterWithTls":        testCreatePravegaClusterWithTls,
-		// "testDeletePods":                         testDeletePods,
+		// "testCreatePravegaClusterWithTls":    testCreatePravegaClusterWithTls,
+		"testDeletePods":             testDeletePods,
 		"testRollbackPravegaCluster": testRollbackPravegaCluster,
 		// "testCreatePravegaClusterWithAuthAndTls": testCreatePravegaClusterWithAuthAndTls,
 	}
