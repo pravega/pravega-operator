b7921a096347feb64fd7206708d3be8b18082936
anishakj <43978302+anishakj@users.noreply.github.com>
2022-04-25 14:07:19 +0530

    Adding support to disable finalizer (#619)
    Signed-off-by: anishakj <anisha.kj@dell.com>

--- main.go
+++ main.go
@@ -18,17 +18,22 @@ package main
 
 import (
 	"context"
+	"errors"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"runtime"
+	"strings"
 
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
 	// to ensure that exec-entrypoint and run can make use of them.
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
 
-	"github.com/operator-framework/operator-lib/leader"
 	controllerconfig "github.com/pravega/bookkeeper-operator/pkg/controller/config"
+	"github.com/pravega/bookkeeper-operator/pkg/util"
 	v1beta1 "github.com/pravega/pravega-operator/api/v1beta1"
 	"github.com/pravega/pravega-operator/controllers"
 	"github.com/pravega/pravega-operator/pkg/version"
@@ -37,6 +42,7 @@ import (
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	//+kubebuilder:scaffold:imports
 )
@@ -51,6 +57,7 @@ var (
 func init() {
 	flag.BoolVar(&versionFlag, "version", false, "Show version and quit")
 	flag.BoolVar(&controllerconfig.TestMode, "test", false, "Enable test mode. Do not use this flag in production")
+	flag.BoolVar(&controllerconfig.DisableFinalizer, "disableFinalizer", false, "Disable finalizers for bookkeeperclusters. Use this flag with awareness of the consequences")
 	flag.BoolVar(&webhookFlag, "webhook", true, "Enable webhook, the default is enabled.")
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 	utilruntime.Must(v1beta1.AddToScheme(scheme))
@@ -86,16 +93,34 @@ func main() {
 		logrus.Warn("----- Running with finalizer disabled. -----")
 	}
 
-	ctx := context.TODO()
-	// Become the leader before proceeding
-	err := leader.Become(ctx, "pravega-operator-lock")
+	// Get a config to talk to the apiserver
+	cfg, err := config.GetConfig()
 	if err != nil {
-		logrus.Error(err)
+		logrus.Fatal(err)
+	}
+
+	operatorNs, err := GetOperatorNamespace()
+	if err != nil {
+		log.Error(err, "failed to get operator namespace")
+		os.Exit(1)
+	}
+
+	namespaces, err := getWatchNamespace()
+	if err != nil {
+		log.Error(err, "unable to get WatchNamespace, "+
+			"the manager will watch and manage resources in all namespaces")
+	}
+
+	// Become the leader before proceeding
+	err = util.BecomeLeader(context.TODO(), cfg, "pravega-operator-lock", operatorNs)
+	if err != nil {
+		log.Error(err, "")
 		os.Exit(1)
 	}
 
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
 		Scheme:             scheme,
+		Namespace:          namespaces,
 		MetricsBindAddress: metricsAddr,
 		Port:               9443,
 	})
@@ -134,4 +159,30 @@ func main() {
 		log.Error(err, "problem running manager")
 		os.Exit(1)
 	}
+}
+
+func GetOperatorNamespace() (string, error) {
+	nsBytes, err := ioutil.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/namespace")
+	if err != nil {
+		if os.IsNotExist(err) {
+			return "", errors.New("file does not exist")
+		}
+		return "", err
+	}
+	ns := strings.TrimSpace(string(nsBytes))
+	return ns, nil
+}
+
+// getWatchNamespace returns the Namespace the operator should be watching for changes
+func getWatchNamespace() (string, error) {
+	// WatchNamespaceEnvVar is the constant for env variable WATCH_NAMESPACE
+	// which specifies the Namespace to watch.
+	// An empty value means the operator is running with cluster scope.
+	var watchNamespaceEnvVar = "WATCH_NAMESPACE"
+
+	ns, found := os.LookupEnv(watchNamespaceEnvVar)
+	if !found {
+		return "", fmt.Errorf("%s must be set", watchNamespaceEnvVar)
+	}
+	return ns, nil
 }
