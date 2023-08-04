009bd6e7a5bfb4bcea59b45e7d8ddf0ed77bd823
anisha.kj <anisha.kj@dell.com>
2023-08-04 02:38:59 -0700

    updated kubernetes version
    Signed-off-by: anisha.kj <anisha.kj@dell.com>

--- test/e2e/resources/kubernetes_master_install.sh
+++ test/e2e/resources/kubernetes_master_install.sh
@@ -28,8 +28,8 @@ echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" \
 && sudo apt-get update
 sudo apt-get update \
 && sudo apt-get install -yq \
-kubelet=1.21.2-00 \
-kubeadm=1.21.2-00 \
+kubelet=1.25.9-00 \
+kubeadm=1.25.9-00 \
 kubernetes-cni
 sudo apt-mark hold kubelet kubeadm kubectl
 UUID=`cat /etc/fstab | grep swap | awk '{print $1}' | tr -d "#UUID="`
